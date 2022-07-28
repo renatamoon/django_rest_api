6f4ca488555e54a8870bd752e680d4395edc91de
Renata Cardoso Monteiro <r.cardoso.monteiro1@gmail.com>
2022-07-28 13:34:37 -0300

    views

--- school/courses/views.py
+++ school/courses/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+# STANDARD IMPORTS
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+# PROJECT IMPORTS
+from .models import Course, Rating
+from .serializers import CourseSerializer, RatingSerializer
+
+
+class CourseAPIView(APIView):
+    """
+    Course API from RM
+    """
+    def get(self, request):
+        courses = Course.objects.all()
+        serializer = CourseSerializer(courses, many=True)
+
+        response = Response(serializer.data)
+
+        return response
+
+
+class RatingAPIView(APIView):
+    """
+    Rating API from RM
+    """
+    def get(self, request):
+        ratings = Rating.objects.all()
+        serializer = RatingSerializer(ratings, many=True)
+
+        response = Response(serializer.data)
+
+        return response
