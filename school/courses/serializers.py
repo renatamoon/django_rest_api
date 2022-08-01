bb4257e71464a4ea5cfbe9aa614fb2be88bdbd60
renatamoon <r.cardoso.monteiro1@gmail.com>
2022-08-01 09:20:03 -0300

    relationships

--- school/courses/serializers.py
+++ school/courses/serializers.py
@@ -30,6 +30,28 @@ class RatingSerializer(serializers.ModelSerializer):
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    # NESTED RELATIONSHIP
+    # many=True - means that a course can have many ratings,
+    # and read_only=True this data is only to read - allowed to method GET
+    # ratings = RatingSerializer(many=True, read_only=True)
+
+    # HYPER LINKED RELATED FIELD
+    """ratings = serializers.HyperlinkedIdentityField(
+        many=True,
+        read_only=True,
+        view_name='rating-detail'
+        # serializer - through the views we do access to the routers
+        # and we need to set the view_name like this, once it's to see the detail
+        # this hyperlink it's seen when you open the router get ... besides seeing all the ratings, it's
+        # shown a hyperlink that you redirect to a page with all the ratings
+    ) """
+
+    # PRIMARY KEY RELATED FIELD
+    """using PrimaryKeyRelatedField it will generate an link but only with the primary key (id) of the rating"""
+    ratings = serializers.PrimaryKeyRelatedField(
+        many=True,
+        read_only=True
+    )
 
     class Meta:
 
@@ -40,7 +62,8 @@ class CourseSerializer(serializers.ModelSerializer):
             'title',
             'url',
             'creation_date',
-            'active'
+            'active',
+            'ratings'
         )
 
 # email and password is a sensitive field, then you can hide it to be shown.
